7c711d7476cdf29b8a97369c9b79cfa97ca02885
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2016-06-01 16:18:52 -0300

    Refactor JS functions

--- js/index.js
+++ js/index.js
@@ -1,33 +1,16 @@
-$("document").ready(function() {
-    var resizeSection = function () {
+var resizeSection = function () {
         var h = $(window).height();
-        $(".view").css("min-height", h + "px");
-    }
+        $(".view").css("min-height", h + "px"); 
+    },
+    scrollTo = function (target) {
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 1000);
+    };
 
-    resizeSection();
+$(window).on('resize load', resizeSection());
 
-    $(window).on('resize', function () {
-        resizeSection();
-    });
-    
-    $("#btnHome").click(function() {
-        $('html, body').animate({
-            scrollTop: $("body").offset().top
-        }, 1000);
-    });
-    $("#btnFolio").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#portfolio").offset().top
-        }, 1000);
-    });
-    $("#btnContact").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#contact").offset().top
-        }, 1000);
-    });
-    $("#btnAbout").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#about").offset().top
-        }, 1000);
-    });
+$('.page-scroll').on('click', function () {
+    var target = $(this).attr("goto");
+    scrollTo(target);
 });
